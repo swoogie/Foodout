27bedf25f0ee20c6b2f0c19dcd90caf77e341947
Ignas <ignasbieksa@gmail.com>
2023-04-08 12:13:17 +0300

    changes to details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'cart',
+    loadComponent: () => import('./cart/cart.page').then( m => m.CartPage)
+  }
 ];
