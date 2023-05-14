bed820ea209564236242aa100cf9a5bacae302b6
Ignas Bieksa <ignasbieksa@gmail.com>
2023-05-14 17:40:35 +0300

    Revert "icon and title"
    This reverts commit 998dd85e274abbd0578778ad3c41b7e4359ef4b1.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,9 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'FoodOut',
+  appName: 'Food application Test',
   webDir: 'www',
-  bundledWebRuntime: false,
+  bundledWebRuntime: false
 };
 
 export default config;
